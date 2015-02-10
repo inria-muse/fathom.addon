e982a6605d29534652df3b48c1ea860780abb908
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-02-10 16:17:54 +0100

    xpi build

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cfx xpi --update-link https://muse.inria.fr/fathom/fathom.xpi --update-url https://muse.inria.fr/fathom/fathom.update.rdf
-cp fathom.xpi ../fathom.web/
-cp fathom.update.rdf ../fathom/web/
+cp -u fathom.xpi ../fathom.web/
+cp -u fathom.update.rdf ../fathom.web/
 pushd ../fathom.web
-git commit -a -m "xpi update"
+git commit -a -m "xpi release update"
 git push
 popd
 
