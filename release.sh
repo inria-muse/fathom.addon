5414c57ddb3cfc7d28972b4678528550db5ca076
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-02-13 19:10:42 +0100

    release script

--- release.sh
+++ release.sh
@@ -3,7 +3,9 @@ cfx xpi --update-link https://muse.inria.fr/fathom/fathom.xpi --update-url https
 cp -u fathom.xpi ../fathom.web/
 cp -u fathom.update.rdf ../fathom.web/
 pushd ../fathom.web
-git commit -a -m "xpi release update"
+git commit -a -m "xpi release"
 git push
 popd
-
+git add fathom.xpi
+git add fathom.update.rdf
+git commit -m "xpi release"
