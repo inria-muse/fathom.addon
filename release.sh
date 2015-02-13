05fd8f982217c2cb154fd095cd5f9c800cd2eab4
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-02-13 19:24:17 +0100

    2.0.3 more upload fixes

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cfx xpi --update-link https://muse.inria.fr/fathom/fathom.xpi --update-url https://muse.inria.fr/fathom/fathom.update.rdf
-cp -u fathom.xpi ../fathom.web/
-cp -u fathom.update.rdf ../fathom.web/
+cp -f fathom.xpi ../fathom.web/
+cp -f fathom.update.rdf ../fathom.web/
 pushd ../fathom.web
 git commit -a -m "xpi release"
 git push
@@ -9,3 +9,4 @@ popd
 git add fathom.xpi
 git add fathom.update.rdf
 git commit -m "xpi release"
+git push
