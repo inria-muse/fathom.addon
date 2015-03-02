69d376820ea8490def4688ccace97f75fdb7e4a7
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-03-02 17:19:21 +0100

    v2.0.12

--- test/test-subprocess.js
+++ test/test-subprocess.js
@@ -11,10 +11,12 @@ exports['test1'] = function(assert, done) {
 	arguments:   ['foo'],
       
 	stdout: function(data) {
-            assert.ok((data.trim() === "foo"), "can read from stdout");
+      	    console.log(data);
+	    assert.ok((data.trim() === "foo"), "can read from stdout");
 	},
 
 	stderr: function(data) {
+	    console.log(data);
             assert.ok(false, "got data on standard error " + data);
 	},
 
@@ -37,10 +39,12 @@ exports['test2'] = function(assert, done) {
 	arguments:   ['ping'],
       
 	stdout: function(data) {
+	    console.log(data);
             assert.ok((data.indexOf("ping")>0), "found ping");
 	},
 
 	stderr: function(data) {
+	    console.log(data);
             assert.ok(false, "got data on standard error " + data);
 	},
 
@@ -57,16 +61,22 @@ exports['test2'] = function(assert, done) {
 
 exports['test3'] = function(assert, done) {
     const child_process = require("sdk/system/child_process");
-	const prog = (os === 'winnt' ? 'echo' : '/bin/echo');
+    const prog = (os === 'winnt' ? 'echo' : '/bin/echo');
+
     var p = child_process.spawn(prog,['foo']);
     p.stdout.on('data', function(data) {
+	console.log(data);
         assert.ok((data.trim() === "foo"), "sdk can read from stdout");
     });
+
     p.stderr.on('data', function(data) {
+	console.log(data);
         assert.ok(false, "sdk got data on standard error " + data);
     });
-    p.on('close', function(code) {
-        assert.ok(code === 0, "sdk return 0 exit code");
+
+    p.on('close', function(res) {
+	console.log(res);
+        assert.ok(res === 0, "sdk return 0 exit code");
         done();
     });
 };
@@ -93,8 +103,8 @@ exports['test4'] = function(assert, done) {
 exports['test5'] = function(assert, done) {
     const child_process = require("sdk/system/child_process");
 	const utils = require('utils');
-	var prog = (os === 'winnt' ? 'C:\\Windows\\System32\\ping.exe' : '/usr/bin/ping');
-    var p = child_process.spawn(prog,['www.google.com']);
+	var prog = (os === 'winnt' ? 'C:\\Windows\\System32\\ping.exe' : '/bin/ping');
+    var p = child_process.spawn(prog,['-c 1','www.google.com']);
 
 	assert.ok(utils.isExecFile(prog), "is executable");
 	
@@ -117,9 +127,9 @@ exports['test6'] = function(assert, done) {
     subprocess.registerDebugHandler(console.log);
     subprocess.registerLogHandler(console.log);
     var p = subprocess.call({
-	command:     (os === 'winnt' ? 'C:\\Windows\\System32\\ping.exe' : '/usr/bin/ping'),
+	command:     (os === 'winnt' ? 'C:\\Windows\\System32\\ping.exe' : '/bin/ping'),
       
-	arguments:   ['www.google.com'],
+	arguments:   ['-c 1','www.google.com'],
       
 	stdout: function(data) {
         assert.ok(data && data.length>0, "ping ok");
