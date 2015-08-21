58e3d1dc7eb39450ab35e62923634c430cad2d6e
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-08-21 14:40:17 +0200

    with version numbering

--- release.sh
+++ release.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+REL=$1
+if [ -z "$REL" ]; then 
+    echo "Usage: release.sh <VERSION>"
+    exit 1
+fi
+
+echo "prepare release=" $REL " ..."
+
+# new xpi
+
 cfx xpi --update-link https://muse.inria.fr/fathom/fathom.xpi --update-url https://muse.inria.fr/fathom/fathom.update.rdf
-cp -f fathom.xpi ../fathom.web/
-cp -f fathom.update.rdf ../fathom.web/
-pushd ../fathom.web
-git commit -a -m "xpi release"
-git push
-popd
+
 git add fathom.xpi
 git add fathom.update.rdf
-git commit -m "xpi release"
+git commit -m "xpi release "$REL
+git tag $REL
 git push
+
+# web release
+
+cp -f fathom.xpi ../fathom.web/
+cp -f fathom.update.rdf ../fathom.web/
+
+pushd ../fathom.web
+git commit -a -m "xpi release "$REL
+git push
+popd
+
+ssh apietila@muse.inria.fr 'cd fathom; git pull;'
+
+echo 'ready'
