a689a9f122309821fcec510f0ac1e4def79b0af4
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-08-26 13:34:38 +0200

    rel script fix

--- release.sh
+++ release.sh
@@ -11,9 +11,7 @@ echo "prepare release=" $REL " ..."
 
 cfx xpi --update-link https://muse.inria.fr/fathom/fathom.xpi --update-url https://muse.inria.fr/fathom/fathom.update.rdf
 
-git add fathom.xpi
-git add fathom.update.rdf
-git commit -m "xpi release "$REL
+git commit -a -m "xpi release "$REL
 git tag $REL
 git push
 
