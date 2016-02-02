2decd31353d99d351a0188946b509b8ebdaa5603
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2016-02-02 18:20:41 +0100

    video qoe debugging

--- data/contentscripts/mediaload.js
+++ data/contentscripts/mediaload.js
@@ -13,98 +13,101 @@
  *
  * @author Anna-Kaisa Pietilainen <anna-kaisa.pietilainen@inria.fr> 
  */
-(function() {
-    function collectEvents(elem, video) {         
-        console.log(elem.baseURI);   
+function collectEvents(elem, video) {         
+    console.log('watch elem ' + elem.baseURI);   
 
-        var ts = new Date();
-        var res = { 
-            ts : ts.getTime(),
-            timezoneoffset : ts.getTimezoneOffset(),
-            base_uri : elem.baseURI,
-            media_src : elem.currentSrc,
-            media_type: (video ? 'video' : 'audio'),
-            media_duration : elem.duration,
-            events : {} 
-        };
-
-        function listenEvent(en) {
-            elem.addEventListener(en, function(e) {
-                if (elem.currentSrc !== res.media_src) {
-                    if (!res.done) {
-                        console.log(res);
-                        self.port.emit('perf', res);                            
-                    }
-
-                    // new video in the same player
-                    ts = new Date();
-                    res = { 
-                        ts : ts.getTime(),
-                        timezoneoffset : ts.getTimezoneOffset(),
-                        base_uri : elem.baseURI,
-                        media_src : elem.currentSrc,
-                        media_type: (video ? 'video' : 'audio'),
-                        media_duration : elem.duration,
-                        events : {} 
-                    };
-                }
-
-                let event = { 
-                    ts : new Date().getTime(), 
-                    buffered : (elem.buffered.length>0 ? [elem.buffered.start(elem.buffered.length-1),elem.buffered.end(elem.buffered.length-1)] : undefined),
-                    position : elem.currentTime,
-                    duration : elem.duration,
-                    parsedFrames : elem.mozParsedFrames,
-                    decodedFrames : elem.mozDecodedFrames,
-                    presentedFrames : elem.mozPresentedFrames,
-                    paintedFrames : elem.mozPaintedFrames,
-                    frameDelay : elem.mozFrameDelay
-                };
-
-                console.log("video event " + en, JSON.stringify(event,null,4));
-                if (!res.events[en])
-                    res.events[en] = [];    
-                res.events[en].push(event);
-
-                if (en === 'ended' || en === 'error') {
-                    res.done = true;
-                    console.log(res);
-                    self.port.emit('perf', res);
-                }
-            });
-        };
-
-        listenEvent("loadstart");
-        listenEvent("progress"); // downloaded bytes
-        listenEvent("suspend");
-        listenEvent("abort");
-        listenEvent("error");            
-        listenEvent("stalled");
-        listenEvent("play");
-        listenEvent("pause");
-        listenEvent("loadedmetadata");
-        listenEvent("loadeddata"); // first data frame received
-        listenEvent("waiting"); // no frames to play
-        listenEvent("playing");
-        listenEvent("canplay");
-        listenEvent("canplaythrough");
-        listenEvent("seeking");
-        listenEvent("seeked");
-//            listenEvent("timeupdate");
-        listenEvent("ratechange");
-        listenEvent("durationchange");
-        listenEvent("ended");
+    var ts = new Date();
+    var res = { 
+        ts : ts.getTime(),
+        timezoneoffset : ts.getTimezoneOffset(),
+        media_src : elem.currentSrc.replace('mediasource:', ''),
+        media_type: (video ? 'video' : 'audio'),
+        media_duration : elem.duration,
+        events : {} 
     };
 
-    setTimeout(function() {
-        var vids = document.getElementsByTagName("video");
-        for (var i = 0; i < vids.length; i++) {
-            collectEvents(vids[i], true);
+    var checkdone = function(reason) {
+        if (!res.done) { 
+            res.done = (reason !== 'pause');
+            console.log(reason,res);
+            if (res.media_src.length>0)
+                self.port.emit('perf', res); 
+            res.events = {}; // in case of suspend, just empty the event queue
         }
+    }
 
-        var audios = document.getElementsByTagName("audio");
-        for (var i = 0; i < audios.length; i++) {
-            collectEvents(audios[i], false);
-        }
-    }, 100);
-}());
+    function listenEvent(en) {
+        elem.addEventListener(en, function(e) {
+            var current = elem.currentSrc.replace('mediasource:','');
+            if (current !== res.media_src) {
+                checkdone('newsrc');
+
+                // new video in the same player
+                ts = new Date();
+                res = { 
+                    ts : ts.getTime(),
+                    timezoneoffset : ts.getTimezoneOffset(),
+                    media_src : current,
+                    media_type: (video ? 'video' : 'audio'),
+                    media_duration : elem.duration,
+                    events : {} 
+                };
+            }
+
+            let event = { 
+                ts : new Date().getTime(), 
+                buffered : (elem.buffered.length>0 ? [elem.buffered.start(elem.buffered.length-1),elem.buffered.end(elem.buffered.length-1)] : undefined),
+                position : elem.currentTime,
+                duration : elem.duration,
+                parsedFrames : elem.mozParsedFrames,
+                decodedFrames : elem.mozDecodedFrames,
+                presentedFrames : elem.mozPresentedFrames,
+                paintedFrames : elem.mozPaintedFrames,
+                frameDelay : elem.mozFrameDelay
+            };
+
+            console.log("video event " + en, JSON.stringify(event,null,4));
+            if (!res.events[en])
+                res.events[en] = [];    
+            res.events[en].push(event);
+
+            if (en === 'ended' || en === 'error' || en === 'pause') {
+                checkdone(en);
+            }
+        });
+    };
+
+    listenEvent("loadstart");
+    listenEvent("progress"); // downloaded bytes
+    listenEvent("suspend");
+    listenEvent("abort");
+    listenEvent("error");            
+    listenEvent("stalled");
+    listenEvent("play");
+    listenEvent("pause");
+    listenEvent("loadedmetadata");
+    listenEvent("loadeddata"); // first data frame received
+    listenEvent("waiting"); // no frames to play
+    listenEvent("playing");
+    listenEvent("canplay");
+    listenEvent("canplaythrough");
+    listenEvent("seeking");
+    listenEvent("seeked");
+//            listenEvent("timeupdate");
+    listenEvent("ratechange");
+    listenEvent("durationchange");
+    listenEvent("ended");
+};
+
+setTimeout(function() {
+    console.log('mediaload.js collect events')
+    var vids = document.getElementsByTagName("video");
+    for (var i = 0; i < vids.length; i++) {
+        collectEvents(vids[i], true);
+    }
+
+    var audios = document.getElementsByTagName("audio");
+    for (var i = 0; i < audios.length; i++) {
+        collectEvents(audios[i], false);
+    }
+}, 10);
